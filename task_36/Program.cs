8d57ca6340f408e14b62de6fa81d7860442fbc65
rena <rena2004.rr@gmail.com>
2022-12-08 20:54:27 +0400

    homework5

---
+++ task_36/Program.cs
@@ -0,0 +1,24 @@
+﻿void Array(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(-100, 100);
+}
+
+int Sum(int[] array) {
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++) {
+        if(i % 2 != 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Sum(array);
+Console.WriteLine($"Результат: {Sum(array)}");
---
+++ task_38/Program.cs
@@ -0,0 +1,36 @@
+﻿void Array(double[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = Math.Round(new Random().NextDouble() * (20 - 10) + 10, 2);
+}
+
+
+double Max(double[] array)
+{
+double max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+if (max < array[i])
+max = array[i];
+}
+return max;
+}
+
+double Min(double[] array)
+{
+double min = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+if (min > array[i])
+min = array[i];
+}
+return min;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+Array(array);
+Console.WriteLine($"Наальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {Max(array) - Min(array)}");
